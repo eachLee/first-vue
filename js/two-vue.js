f58549d878fada32db62163958f31ddd3e59bd7d
eachLee <eachli@163.com>
2018-10-25 18:07:50 +0800

    vue test

--- js/two-vue.js
+++ js/two-vue.js
@@ -1,11 +1,97 @@
-var obj = {
-	foo: 'bar'
-};
-Object.freeze(obj);
-var vm = new Vue({
-	el: '#app',
-	data: obj,
+var watchExampleVM = new Vue({
+	el: '#watch-example',
+	data: {
+		question: '',
+		answer: 'I cannot give you an answer until you ask a question!'
+	},
+	watch: {
+		// 如果 `question` 发生改变，这个函数就会运行
+		question: function (newQuestion, oldQuestion) {
+			this.answer = 'Waiting for you to stop typing...';
+			this.debouncedGetAnswer();
+		}
+	},
 	created: function () {
-		console.log('foo is ' + this.foo);
+		// `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+		// 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+		// AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+		// `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+		// 请参考：https://lodash.com/docs#debounce
+		this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
+	},
+	methods: {
+		getAnswer: function () {
+			if (this.question.indexOf('?') === -1) {
+				this.answer = 'Questions usually contain a question mark. ;-)';
+				return;
+			}
+			this.answer = 'Thinking...';
+			var vm = this;
+			axios.get('https://yesno.wtf/api')
+				.then(function (response) {
+					vm.answer = _.capitalize(response.data.answer);
+				})
+				.catch(function (error) {
+					vm.answer = 'Error! Could not reach the API. ' + error;
+				});
+		}
+	}
+});
+var app = new Vue({
+	el: '#app',
+	data: {
+		isActive: true,
+		error: null
+	},
+	computed: {
+		classObject: function () {
+			return {
+				active: this.isActive && !this.error,
+				'text-danger': this.error && this.error.type === 'fatal'
+			};
+		}
+	}
+});
+var app2 = new Vue({
+	el: '#app2',
+	data: {
+		activeClass: 'active',
+		errorClass: 'text-danger'
+	}
+});
+var app3 = new Vue({
+	el: '#app3',
+	data: {
+		activeColor: 'red',
+		fontSize: 30
+	}
+});
+var app4 = new Vue({
+	el: '#app4'
+});
+var app5 = new Vue({
+	el: '#app5',
+	data: {
+		ok: true
+	}
+});
+var example2 = new Vue({
+	el: '#example-2',
+	data: {
+		parentMessage: 'Parent',
+		items: [
+			{ message: 'Foo' },
+			{ message: 'Bar' }
+		]
+	}
+});
+new Vue({
+	el: '#v-for-object',
+	data: {
+		object: {
+			firstName: 'John',
+			lastName: 'Doe',
+			age: 30
+		}
 	}
 });
